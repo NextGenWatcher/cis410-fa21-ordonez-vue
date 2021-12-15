5cd5142d37beb5d3a0d782242fe1486a5871222b
Giezi Ordonez <yessy@rams.colostate.edu>
2021-12-14 19:20:42 -0700

    finished makeing it awesome

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,60 @@
 <template>
   <div>
     <h1>Home</h1>
+    <br />
+    <p>Buddy the Elf, whats you favorite color?</p>
+    <p>
+      Elf is my favorite Christmas movie for sure and thank you for an awesome
+      semester!
+    </p>
   </div>
+  <body>
+<center>
+    <!-- The image has scrolling behavior to left -->
+    <marquee  behavior="scroll" direction="left">        
+         <iframe
+    src="https://giphy.com/embed/oBTCwXvs0SUNO"
+    width="480"
+    height="313"
+    frameBorder="0"
+    class="giphy-embed"
+    allowFullScreen
+  ></iframe>
+    </marquee>  
+      
+   <!-- The image has scrolling behavior to the upper direction. -->
+   <marquee  behavior="scroll" direction="up">         
+        <iframe
+    src="https://giphy.com/embed/l1L0hvPiriGNqao80"
+    width="480"
+    height="270"
+    frameBorder="0"
+    class="giphy-embed"
+    allowFullScreen
+  ></iframe>
+    </marquee>  
+</center>
+</body>
+<center>
+    <marquee  behavior="alternate" direction="left">          
+          <iframe
+    src="https://giphy.com/embed/l2YWG61x3ovPaIJb2"
+    width="480"
+    height="270"
+    frameBorder="0"
+    class="giphy-embed"
+    allowFullScreen
+    margin:
+  ></iframe>
+    </marquee>  
+      
+   <marquee  behavior="alternate" direction="right">         
+         <iframe src="https://giphy.com/embed/3ofT5JfhISfBF1BhOo" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </marquee>  
+</center>
+  
+  
+  
 </template>
-<script></script>
 
 <style></style>
